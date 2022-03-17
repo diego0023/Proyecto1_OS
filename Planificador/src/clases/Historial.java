c5b7d97eeda7e7924b4941ef5e11ab63d60d0dc5
Vinicio <vinicioricci@gmail.com>
2022-03-16 21:21:00 -0600

    Inicio de  clase historial
    Se establecio un constructor donde se planea obtener los datos para el historial

---
+++ Planificador/src/clases/Historial.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clases;
+
+import java.time.LocalDateTime;
+
+/**
+ *
+ * @author User
+ */
+public class Historial {
+
+    
+    Procesos proces = new Procesos();
+    private int id;
+    private String tiempo_inicial;
+    private String tiempo_final;
+    public Historial() {
+        this.id= proces.getId();
+        
+    }
+    
+    
+    
+    
+}
