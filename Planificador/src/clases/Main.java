648132928002743b6064521cf94d29628d9b4665
Hervert Damian Martinez <luis2009hervert@gmail.com>
2022-03-17 19:58:21 -0600

    Reloj
    Se añadio reloj y funcion para guardar el tiempo

--- Planificador/src/clases/Main.java
+++ Planificador/src/clases/Main.java
@@ -1,6 +1,7 @@
 package clases;
 
 import java.beans.PropertyChangeSupport;
+import java.util.Calendar;
 import java.util.Random;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -20,10 +21,14 @@ public class Main extends javax.swing.JFrame {
     int id = 2;
     // objeto del procesador
     procesador cpu = new procesador();
-
+    // Reloj 
+    Reloj horaActual = new Reloj();
+    //Solicitar Tiempo
+    boolean FTime=false;
     public Main() {
         initComponents();
         cpu.start();
+        horaActual.start();
     }
 
     public int getRandom() {
@@ -41,8 +46,9 @@ public class Main extends javax.swing.JFrame {
         Procesos aux2 = new Procesos();
         // contador de programa (que espacio de memoria esta en ejecucion)
         int pc;
-       // variable para cambiar de proceso
+        // variable para cambiar de proceso
         int index;
+
         @Override
         public void run() {
             // proceso infinito para que no se muera el hilo 
@@ -76,7 +82,7 @@ public class Main extends javax.swing.JFrame {
                         lbBase.setText(String.valueOf(aux.getPosinicialHex()));
                         lbLimite.setText(String.valueOf(aux.getPosfinalHex()));
                         pc = aux.getPosinicial();
-                        System.out.println("Proceso " + aux.getId() );
+                        System.out.println("Proceso " + aux.getId());
                         while ((quantum > 0) && (aux.getTamanioRestante() > 0)) {
                             // poner la instruccion ejecutandose
                             lbContadorPrograma.setText(convert.convertir(pc));
@@ -87,7 +93,7 @@ public class Main extends javax.swing.JFrame {
                             }
                             quantum--;
                             System.out.println("Quantum: " + quantum);
-                            System.out.println("Tiempo restante: " +aux.getTamanioRestante() );
+                            System.out.println("Tiempo restante: " + aux.getTamanioRestante());
                             pc++;
                             aux.setTamanioRestante(aux.getTamanioRestante() - 1);
 
@@ -100,17 +106,17 @@ public class Main extends javax.swing.JFrame {
                          */
                         // pasar al siguiente proceso, ver si el el ultimo
                         if (memoria.isLast(memoria.ProcesoActual)) {
-                           // esta en el ultimo proceso pasa al primero/ si solo hay uno da vueltas
+                            // esta en el ultimo proceso pasa al primero/ si solo hay uno da vueltas
                             memoria.setProcesoActual(memoria.proceso.getFirst());
                         } else {
                             // pasamos al siguiente 
-                           
+
                             aux2 =  memoria.getNext(aux);
                             memoria.setProcesoActual(aux2);
-                            
+
                             
                         }
-                        
+
                         // ver si es necesario eliminar el proceso
                         if (aux.getTamanioRestante() == 0) {
                             memoria.proceso.remove(aux);
@@ -166,6 +172,8 @@ public class Main extends javax.swing.JFrame {
         lbLimite = new javax.swing.JLabel();
         jLabel7 = new javax.swing.JLabel();
         lbActivador = new javax.swing.JLabel();
+        Hora = new javax.swing.JLabel();
+        WTime = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -255,20 +263,37 @@ public class Main extends javax.swing.JFrame {
 
         jLabel7.setText("ACTIVADOR:");
 
+        Hora.setText("Hora de Sistema");
+
+        WTime.setText("Mostrar Hora");
+        WTime.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                WTimeActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(21, 21, 21)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addComponent(btnProceso)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
-                        .addComponent(jLabel7)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(lbActivador, javax.swing.GroupLayout.PREFERRED_SIZE, 64, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addGap(73, 73, 73)
-                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(21, 21, 21)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addComponent(btnProceso)
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(jLabel7)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(lbActivador, javax.swing.GroupLayout.PREFERRED_SIZE, 64, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                        .addGap(73, 73, 73)
+                        .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(0, 0, Short.MAX_VALUE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addComponent(Hora, javax.swing.GroupLayout.PREFERRED_SIZE, 112, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(WTime)))
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
@@ -285,11 +310,63 @@ public class Main extends javax.swing.JFrame {
                     .addGroup(layout.createSequentialGroup()
                         .addGap(30, 30, 30)
                         .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addContainerGap(57, Short.MAX_VALUE))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(Hora)
+                        .addContainerGap())
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(18, 18, 18)
+                        .addComponent(WTime)
+                        .addContainerGap(15, Short.MAX_VALUE))))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    // Hilo del Reloj
+    public class Reloj extends Thread {
+
+        Calendar calendario;
+
+        @Override
+        public void run() {
+
+            while (true) {
+                String horaSistema = "";
+                calendario = Calendar.getInstance();
+
+                if (calendario.get(Calendar.HOUR_OF_DAY) < 10) {
+                    horaSistema += String.valueOf("0" + calendario.get(Calendar.HOUR_OF_DAY)) + ":";
+                } else {
+                    horaSistema += String.valueOf(calendario.get(Calendar.HOUR_OF_DAY)) + ":";
+                }
+                if (calendario.get(Calendar.MINUTE) < 10) {
+                    horaSistema += String.valueOf("0" + calendario.get(Calendar.MINUTE)) + ":";
+                } else {
+                    horaSistema += String.valueOf(calendario.get(Calendar.MINUTE)) + ":";
+                }
+                if (calendario.get(Calendar.SECOND) < 10) {
+                    horaSistema += String.valueOf("0" + calendario.get(Calendar.SECOND)) + ":";
+                } else {
+                    horaSistema += String.valueOf(calendario.get(Calendar.SECOND)) + ":";
+                }
+                horaSistema += String.valueOf(calendario.get(Calendar.MILLISECOND)) + " hrs";
+                Hora.setText(horaSistema);
+                if(FTime==true)
+                {
+                    JOptionPane.showMessageDialog(null, "La Hora es: " + horaSistema);
+                    FTime=false;
+                }
+                try {
+                    Thread.sleep(1);
+                } catch (InterruptedException ex) {
+                    Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        }
+   
+    }
 
     private void btnProcesoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnProcesoActionPerformed
         // crear un nuevo proceso
@@ -337,6 +414,11 @@ public class Main extends javax.swing.JFrame {
         }
     }//GEN-LAST:event_btnProcesoActionPerformed
 
+    private void WTimeActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_WTimeActionPerformed
+        // TODO add your handling code here:
+        FTime=true;
+    }//GEN-LAST:event_WTimeActionPerformed
+
     /**
      * @param args the command line arguments
      */
@@ -373,6 +455,8 @@ public class Main extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel Hora;
+    private javax.swing.JButton WTime;
     private javax.swing.JButton btnProceso;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
